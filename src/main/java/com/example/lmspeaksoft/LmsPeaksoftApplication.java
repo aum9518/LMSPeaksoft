f96d5bc625db2407941e1000b83c14a113ceefd0
manasabduganiuulu <abduganiuullumanasbek@gmail.com>
2023-07-07 12:41:18 +0600

    add

---
+++ src/main/java/com/example/lmspeaksoft/LmsPeaksoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.lmspeaksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsPeaksoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsPeaksoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lmspeaksoft/LmsPeaksoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lmspeaksoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsPeaksoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
