a10dabf8effe8841369bf3c27de14ba90a1286ad
manasabduganiuulu <abduganiuullumanasbek@gmail.com>
2023-07-13 13:12:27 +0600

    add instructorCRUD

--- src/main/java/com/example/lmspeaksoft/service/InstructorService.java
+++ src/main/java/com/example/lmspeaksoft/service/InstructorService.java
@@ -3,6 +3,7 @@ package com.example.lmspeaksoft.service;
 import com.example.lmspeaksoft.dto.SimpleResponse;
 import com.example.lmspeaksoft.dto.instructor.instructorReq.InstructorRequest;
 import com.example.lmspeaksoft.dto.instructor.instructorRes.InstructorResponse;
+import com.example.lmspeaksoft.dto.pagination.PaginationInstructorResponse;
 
 public interface InstructorService {
     SimpleResponse saveInstructor(InstructorRequest instructorRequest);
@@ -11,7 +12,7 @@ public interface InstructorService {
 
     SimpleResponse updateInstructor(Long id, InstructorRequest instructorRequest);
 
-    InstructorResponse getAllInstructor();
+    PaginationInstructorResponse getAllInstructor(int currentPage, int pageSize);
 
     SimpleResponse deleteInstructorById(Long id);
 
